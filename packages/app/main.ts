e0130612f63a32ed9e87daebd23a5ae2dfa72b71
t.kuriyama <koolii0909@gmail.com>
2022-04-11 11:29:14 +0900

    yarn workspaces, incremental build, no alias importing

---
+++ packages/app/main.ts
@@ -0,0 +1,5 @@
+import { max, subtract, sum } from "shared"
+
+console.log("shared/max at app", max(1, 2))
+console.log("shared/subtract at app", subtract(100, 40))
+console.log("shared/sum at app", sum(10, 40))
